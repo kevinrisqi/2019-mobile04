cf7a6889473d148fa59f23845e265e407f4ea465
Kevin <kevinrisqi22@gmail.com>
2019-09-16 14:11:05 +0700

    Membuat Explicit Intent

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -7,12 +7,23 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class ExplicitIntentActivity extends AppCompatActivity {
+
+    private TextView outputText;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        outputText = findViewById(R.id.text_output);
+        editText = findViewById(R.id.input_name);
     }
 
+    public void cetak(View view) {
+        String nameInput = editText.getText().toString();
+        outputText.setText(nameInput);
+    }
 }
